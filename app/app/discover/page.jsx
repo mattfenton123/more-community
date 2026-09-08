02b2e467306d8bbbef02acf981d5e8484f993bb6
Matt <matt@inspiredventures.co.uk>
2026-09-08 16:21:33 +0100

    fix: resolve discover feed syntax error

--- app/app/discover/page.jsx
+++ app/app/discover/page.jsx
@@ -537,6 +537,7 @@ export default function Discover() {
                         </button>
                       </div>
                     </div>
+                  </div>
                   </React.Fragment>
                 );
               })}
